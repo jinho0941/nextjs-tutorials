2be94a99ea497873178625472f17ef3add818273
daggax <daggax8235@gmail.com>
2024-07-26 07:59:20 +0900

    오타수정

--- app/(layout)/(main)/page.tsx
+++ app/(layout)/(main)/page.tsx
@@ -7,8 +7,7 @@ export default async function Home() {
     <section className='px-8 mt-10'>
       <h1 className='text-3xl font-bold'>NEXT 13 tutorials</h1>
       <p className='mt-5 text-lg font-semibold'>
-        튜토리얼을 통해 server component와 server action 에 대한 이해도를
-        올려보세요!
+        튜토리얼을 통해 server component와 ssr 에 대한 이해도를 올려보세요!
       </p>
       <Button asChild className='mt-4'>
         <Link href={'/docs'}>시작하기</Link>
